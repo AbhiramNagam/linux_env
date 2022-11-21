d9ddc8ba4ae689a795a647a755f7e62e40bc0864
Abhiram Nagam <93092983+AbhiramNagam@users.noreply.github.com>
2022-11-21 03:49:35 +0000

    changes

---
+++ pp/m2_6.py
@@ -0,0 +1,10 @@
+import pandas as pd
+dataset={
+    "col1":[1,2,3,4,5],
+    "col2":[10,20,30,40,50],
+    "col3":[100,200,300,400,500]
+}
+df=pd.DataFram
+print(df)
+ds=pd.Series(df.col1)
+print(ds)
